e9e95013a265835bf9eddfc508bc40e9ee90df26
Ali Görkem Aksöz <a.gorkemaksoz@gmail.com>
2026-03-16 21:36:40 +0300

    Initial commit

---
+++ LuminaFlow/LuminaFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuminaFlowApp.swift
+//  LuminaFlow
+//
+//  Created by Ali Görkem Aksöz on 16.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct LuminaFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
